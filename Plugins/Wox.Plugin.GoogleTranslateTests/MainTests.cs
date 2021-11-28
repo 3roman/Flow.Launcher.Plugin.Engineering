edb67a0718aebbc284207421b1d62e2594fae276
3roman <595347046@qq.com>
2021-11-28 23:07:48 +0800

    :sparkles:完成GoogleTranslate核心功能

---
+++ Plugins/Wox.Plugin.GoogleTranslateTests/MainTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Wox.Plugin.GoogleTranslate.Tests
+{
+    [TestClass()]
+    public class MainTests
+    {
+        [TestMethod()]
+        public void IsChineseTest()
+        {
+            var ret = new Main().IsChinese("你好，世界");
+            Assert.IsTrue(ret);
+
+            ret = new Main().IsChinese("hello world");
+            Assert.IsFalse(ret);
+        }
+    }
+}
