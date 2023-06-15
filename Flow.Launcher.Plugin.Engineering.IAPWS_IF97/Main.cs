1a53326ec032056e3338057d27be852c5162c53b
guga <gugamusk@gmail.com>
2023-06-15 21:24:22 +0800

    :hammer: migrate to Flow.Launcher infrastructure

---
+++ Flow.Launcher.Plugin.Engineering.IAPWS_IF97/Main.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+
+namespace Flow.Launcher.Plugin.Engineering.IAPWS_IF97
+{
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+
+            if (0 == string.Compare("pt", query.FirstSearch, true) && 3 == query.SearchTerms.Length)
+            {
+                _ = double.TryParse(query.SecondSearch, out double p);
+                _ = double.TryParse(query.ThirdSearch, out double t);
+
+                results = new List<Result>
+                {
+                    new Result
+                    {
+                        Title = IF97.PT2H(p, t) + string.Empty,
+                        SubTitle = "Specific Enthalpy (H)   [kJ/kg]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2S(p, t) + string.Empty,
+                        SubTitle = "Specific Entropy (S)   [kJ/(kg.℃)]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2V(p, t) + string.Empty,
+                        SubTitle = "Specific Volume (V)   [m³/kg]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2CP(p, t) + string.Empty,
+                        SubTitle = "Specific Isobaric Heat Capacity (CP)   [kJ/(kg.℃)]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2CV(p, t) + string.Empty,
+                        SubTitle = "Specific Isochoric  Heat Capacity (CV)   [kJ/(kg.℃)]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2DV(p, t) * 1E3 + string.Empty,
+                        SubTitle = "Dynamic Viscosity (DV)   [cp]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2KS(p, t) + string.Empty,
+                        SubTitle = "Isentropic Exponent (KS)",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2E(p, t)  + string.Empty,
+                        SubTitle = "Specific Exergy (E)   [kJ/kg]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2Z(p, t) + string.Empty,
+                        SubTitle = "Compressibility Factor (Z)",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2JTC(p, t) + string.Empty,
+                        SubTitle = "Joule-Thomson Coefficient (JTC)",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2X(p, t) + string.Empty,
+                        SubTitle = "Steam Quality (X)",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PT2R(p, t) + string.Empty,
+                        SubTitle = "Region (R)",
+                    },
+                };
+            }
+            else if (0 == string.Compare("px", query.FirstSearch, true) && 3 == query.SearchTerms.Length)
+            {
+                _ = double.TryParse(query.SecondSearch, out double p);
+                _ = double.TryParse(query.ThirdSearch, out double x);
+
+                results = new List<Result>
+                {
+                    new Result
+                    {
+                        Title = IF97.PX2T(p, x) + string.Empty,
+                        SubTitle = "T  [℃]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2H(p, x) + string.Empty,
+                        SubTitle = "Specific Enthalpy (H)   [kJ/kg]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2S(p, x) + string.Empty,
+                         SubTitle = "Specific Entropy (S)   [kJ/(kg.℃)]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2V(p, x) + string.Empty,
+                        SubTitle = "Specific Volume (V)   [m³/kg]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2CP(p, x) + string.Empty,
+                        SubTitle = "Specific Isobaric Heat Capacity (CP)   [kJ/(kg.℃)]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2CV(p, x) + string.Empty,
+                        SubTitle = "Specific Isochoric  Heat Capacity (CV)   [kJ/(kg.℃)]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2DV(p, x) * 1E3 + string.Empty,
+                        SubTitle = "Dynamic Viscosity (DV)   [cp]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2KS(p, x) + string.Empty,
+                        SubTitle = "Isentropic Exponent (KS)",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2E(p, x)  + string.Empty,
+                        SubTitle = "Specific Exergy (E)   [kJ/kg]",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2Z(p, x) + string.Empty,
+                        SubTitle = "Compressibility Factor (Z)",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2JTC(p, x) + string.Empty,
+                        SubTitle = "Joule-Thomson Coefficient (JTC)",
+                    },
+                    new Result
+                    {
+                        Title = IF97.PX2R(p, x) + string.Empty,
+                        SubTitle = "Region (R)",
+                    },
+                };
+            }
+
+            results.ForEach(item => item.Action =
+                    _ =>
+                    {
+                        System.Windows.Clipboard.SetDataObject(item.Title);
+                        return false;
+                    }
+                 );
+
+            return results;
+        }
+    }
+}
---
+++ Flow.Launcher.Plugin.Engineering.IAPWS_IF97/SEUIF97.cs
@@ -0,0 +1,245 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Flow.Launcher.Plugin.Engineering.IAPWS_IF97
+{
+    public class IF97
+    {
+        public static double PT2V(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 3);
+        }
+
+        public static double PT2H(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 4);
+        }
+
+        public static double PT2S(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 5);
+        }
+
+        public static double PT2E(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 6);
+        }
+
+        public static double PT2CP(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 8);
+        }
+
+        public static double PT2CV(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 9);
+        }
+
+        public static double PT2W(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 10);
+        }
+
+        public static double PT2KS(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 11);
+        }
+
+        public static double PT2Z(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 14);
+        }
+
+        public static double PT2X(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 15);
+        }
+
+        public static double PT2R(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 16);
+        }
+
+        public static double PT2JTC(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 23);
+        }
+
+        public static double PT2DV(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 24);
+        }
+
+        public static double PT2TC(double p, double t)
+        {
+            return SEUIF97.seupt(p / 10, t, 26) * 1000;
+        }
+
+        public static double PX2V(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 3);
+        }
+
+        public static double PX2H(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 4);
+        }
+
+        public static double PX2S(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 5);
+        }
+
+        public static double PX2E(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 6);
+        }
+
+        public static double PX2CP(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 8);
+        }
+
+        public static double PX2CV(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 9);
+        }
+
+        public static double PX2W(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 10);
+        }
+
+        public static double PX2KS(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 11);
+        }
+
+        public static double PX2Z(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 14);
+        }
+
+        public static double PX2T(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 1);
+        }
+
+        public static double PX2R(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 16);
+        }
+
+        public static double PX2JTC(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 23);
+        }
+
+        public static double PX2DV(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 24);
+        }
+
+        public static double PX2TC(double p, double x)
+        {
+            return SEUIF97.seupx(p / 10, x, 26);
+        }
+
+        public static double TX2V(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 3);
+        }
+
+        public static double TX2H(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 4);
+        }
+
+        public static double TX2S(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 5);
+        }
+
+        public static double TX2E(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 6);
+        }
+
+        public static double TX2CP(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 8);
+        }
+
+        public static double TX2CV(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 9);
+        }
+
+        public static double TX2W(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 10);
+        }
+
+        public static double TX2KS(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 11);
+        }
+
+        public static double TX2Z(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 14);
+        }
+
+        public static double TX2P(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 0) * 10;
+        }
+
+        public static double TX2R(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 16);
+        }
+
+        public static double TX2JTC(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 23);
+        }
+
+        public static double TX2DV(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 24);
+        }
+
+        public static double TX2TC(double t, double x)
+        {
+            return SEUIF97.seutx(t, x, 26);
+        }
+    }
+
+    public class SEUIF97
+    {
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seupt(double p, double t, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seuph(double p, double h, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seups(double p, double s, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seupv(double p, double v, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seuth(double t, double h, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seuts(double t, double s, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seutv(double t, double v, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seuhs(double h, double s, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seupx(double p, double x, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seutx(double t, double x, int wid);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seuishd(double pi, double ti, double pe);
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
+        public static extern double seuief(double pi, double ti, double pe, double te);
+    }
+}
